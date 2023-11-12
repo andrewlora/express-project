0d0b36a7bc861608c4605706076d5fad6ee4c52f
Andres Lora <jalc.andres.lora@gmail.com>
2023-11-12 14:21:45 -0700

    added watch script to run nodemon automatically and added new middleware

--- server.js
+++ server.js
@@ -13,7 +13,18 @@ const friends = [
     id: 1,
     name: "Sir Isaac Newton",
   },
+  {
+    id: 2,
+    name: "Nikola Tesla",
+  },
 ];
+
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello");
