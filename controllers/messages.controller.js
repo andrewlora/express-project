7356b385838af5c98bda2dd59f5b483eebfe15cd
Andres Lora <jalc.andres.lora@gmail.com>
2023-11-13 14:42:31 -0700

    Sending file with res.sendFile and __dirname with path module

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,7 @@
+const path = require("path");
 function getMessages(req, res) {
-  res.send("<ul><li>Welcome to the Jungle !!!!</li></ul>");
+  //res.send("<ul><li>Welcome to the Jungle !!!!</li></ul>");
+  res.sendFile(path.join(__dirname, "..", "public", "skimountain.jpg"));
 }
 
 function postMessages(req, res) {
